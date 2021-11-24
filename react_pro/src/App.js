f4adced695493013959262b4b57aeded2859e096
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2021-11-24 20:28:50 +0100

    Navbar (Last)
    Navbar is done and ready to go

--- react_pro/src/App.js
+++ react_pro/src/App.js
@@ -1,11 +1,13 @@
 import React  from 'react';
 import Navbar from "./components/navbar/Navbar";
 import './App.css';
+import Home from './components/Pages';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
+     
     </div>
   );
 }
---
+++ react_pro/src/components/Pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+const Home = () => {
+    return (
+        <>
+            
+            <Navbar />
+        </>
+    )
+}
+
+export default Home
