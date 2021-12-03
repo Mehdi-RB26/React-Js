09e7cebc670607bb9d880575529908705373c950
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2021-12-03 03:53:50 +0100

    SignIn

--- react_pro/src/components/Pages/signin.js
+++ react_pro/src/components/Pages/signin.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import SignIn from '../Signin'
 
 const SigninPage = () => {
     return (
-        <div>
-            <h1>Sign In Page</h1>
-        </div>
+        <>
+            <SignIn />
+        </>
     )
 }
 
